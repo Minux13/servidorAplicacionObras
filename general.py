6241bb86f5786c3fda60976eb6a9c1ab4fa480c5
Miriam Reyes <miriam.physics@gmail.com>
2019-06-05 23:08:12 -0500

    Archivo para los titulos y urls de los doc

---
+++ general.py
@@ -0,0 +1,16 @@
+menuContract = [
+    {'name':'Contratos',  'url':'href=/contracts ', 'nameSingular': 'Contrato'  },
+    {'name':'Contratos',  'url':'href=/contracts ','isActive':'active'},
+    {'name':'Proyectos',  'url':'href=/projects ' ,'isActive':''},
+    {'name':'Proveedores','url':'href=/providers ','isActive':''},
+    {'name':'Tableros',   'url':'href=/contracts ','isActive':''},
+]
+
+
+menuProvider = [
+    {'name':'Proveedores','url':'href=/providers ', 'nameSingular': 'Proveedor'  },
+    {'name':'Contratos',  'url':'href=/contracts ','isActive':''},
+    {'name':'Proyectos',  'url':'href=/projects  ' ,'isActive':''},
+    {'name':'Proveedores','url':'href=/providers ','isActive':'active'},
+    {'name':'Tableros',   'url':'href=/contracts ','isActive':''},
+]
