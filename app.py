a14b86bae15a4b8e52b0b07cfa9c5b6c29fd11c7
Miriam Reyes <miriam.physics@gmail.com>
2019-06-07 17:22:54 -0500

    App file

--- app.py
+++ app.py
@@ -125,8 +125,6 @@ def contracts():
     else:
         data = request.get_json()
         
-        print("////////////")
-        print(data)
         paginationStart     = data['paginationStart']
         paginationStep      = data['paginationStep']
         paginationBy        = data['by']
@@ -142,11 +140,6 @@ def contracts():
         else:
             urlCount = URLFrp + 'contracts/count?' + searchBy + '=' + valueSearchBy
             searchQuery = '&'+ searchBy +'=' + valueSearchBy
-
-
-        print("////////////")
-        print(urlCount)
-        print( 'contracts/?offset=' + str(paginationStart) + '&limit=' + str(paginationStep) + searchQuery)
 
 
         countAmount = requests.get( urlCount ).json()['count']
@@ -169,7 +162,21 @@ def contractsAdd():
     #Renderiza el template del formulario para agregar un proveedor
     if request.method == 'GET':
         
-        return render_template( 'contracts/add.html', catalog='contracts', menu = general.menuContract )
+        
+        #Obtiene todos providers para el select
+        urlCountProviders = URLFrp + 'providers/count'
+        countAmountProviders = requests.get( urlCountProviders ).json()['count']
+
+        queryStrProviders = 'providers/?offset=0&limit=' + str(countAmountProviders)
+        urlProviders = URLFrp + queryStrProviders
+
+        rProviders = requests.get( urlProviders ) 
+        providers = rProviders.json() 
+
+
+
+
+        return render_template( 'contracts/add.html', catalog='contracts', menu = general.menuContract, providers =providers )
     
     #Cuando termina de cargar la pagina el javascrip pide la lista de los proveedores
     else:
@@ -195,10 +202,10 @@ def contractsAdd():
         }
 
         dataJSON = json.dumps(dicData)
-        print(dicData['title'])
+
         url = URLFrp + 'contracts/'
         r = requests.post( url, data=dataJSON)
-        print(r)
+
         return jsonify( data = data )
 
 
@@ -214,7 +221,19 @@ def contractsEdit(provider_id):
         r = requests.get( url) 
         reqJ = r.json()
 
-        return render_template( 'contracts/edit.html', data = reqJ, catalog = 'contracts', menu = general.menuContract )
+        #Obtiene todos providers
+        urlCountProviders = URLFrp + 'providers/count'
+        countAmountProviders = requests.get( urlCountProviders ).json()['count']
+
+        queryStrProviders = 'providers/?offset=0&limit=' + str(countAmountProviders)
+        urlProviders = URLFrp + queryStrProviders
+
+        rProviders = requests.get( urlProviders ) 
+        providers = rProviders.json() 
+        
+        
+
+        return render_template( 'contracts/edit.html', data = reqJ, catalog = 'contracts', menu = general.menuContract, providers = providers )
     
     #Cuando termina de cargar la pagina el javascrip pide la lista de los proveedores
     elif request.method == 'POST':
@@ -246,7 +265,7 @@ def contractsEdit(provider_id):
         url = URLFrp + 'contracts/' + idRegister
         r = requests.put( url, data=dataJSON)
 
-        return jsonify( {'success':'success'} )
+        return jsonify( {'status_code': r.status_code} )
 
     elif request.method == 'DELETE':
         url = URLFrp + 'contracts/' + str(provider_id)
@@ -255,6 +274,152 @@ def contractsEdit(provider_id):
 
 
 
+
+
+
+########   Project   #######3
+
+#Paginacion
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+
+    #Renderiza el template de la lista de proveedores
+    if request.method == 'GET':
+        
+        return render_template( 'projects/index.html', catalog='projects', menu = general.menuProject, title='Obras' )
+    
+    else:
+        data = request.get_json()
+        
+        paginationStart     = data['paginationStart']
+        paginationStep      = data['paginationStep']
+        paginationBy        = data['by']
+        paginationOrder     = data['order']
+        searchBy            = data['searchBy']
+        valueSearchBy       = data['valueSearchBy']
+        
+
+        if searchBy == '':
+            urlCount = URLFrp + 'projects/count'
+            searchQuery = '' 
+        else:
+            urlCount = URLFrp + 'projects/count?' + searchBy + '=' + valueSearchBy
+            searchQuery = '&'+ searchBy +'=' + valueSearchBy
+
+
+        countAmount = requests.get( urlCount ).json()['count']
+        queryStr = 'projects/?offset=' + str(paginationStart) + '&limit=' + str(paginationStep) + searchQuery
+        url = URLFrp + queryStr
+
+
+        r = requests.get( url) 
+        dataRes = r.json() 
+        
+        return jsonify( { 'data' : dataRes, 'count' : countAmount} )
+
+
+
+#Agrega project
+@app.route('/projects/add', methods=['GET', 'POST'])
+def projectsAdd():
+
+    #Renderiza el template del formulario para agregar un 
+    if request.method == 'GET':
+        
+        #Obtiene todos contracts para el select
+        urlCountContracts = URLFrp + 'contracts/count'
+        countAmountContracts = requests.get( urlCountContracts ).json()['count']
+
+        queryStrContracts = 'contracts/?offset=0&limit=' + str(countAmountContracts)
+        urlContracts = URLFrp + queryStrContracts
+
+        rContracts = requests.get( urlContracts ) 
+        contracts = rContracts.json() 
+        
+
+        return render_template( 'projects/add.html', catalog='projects', menu = general.menuProject, contracts = contracts )
+    
+
+    #Cuando termina de cargar la pagina el javascrip pide la lista de los proveedores
+    else:
+        data = request.get_json()
+
+        dicData = { 
+            'title'         : data['title'         ], 
+            'description'   : data['description'   ], 
+            'city'          : data['city'          ], 
+            'category'      : data['category'      ], 
+            'department'    : data['department'    ], 
+            'budget'        : data['budget'        ], 
+            'contract'      : data['contract'      ], 
+            'planed_kickoff': data['planed_kickoff'], 
+            'planed_ending' : data['planed_ending' ], 
+            'inceptor_uuid' : data['inceptor_uuid' ], 
+        }
+
+        dataJSON = json.dumps(dicData)
+
+        url = URLFrp + 'projects/'
+        r = requests.post( url, data=dataJSON)
+
+        return jsonify( data = data )
+
+
+
+#Edita projects
+@app.route('/projects/edit/<int:provider_id>', methods=['GET', 'POST', 'DELETE'])
+def projectsEdit(provider_id):
+
+    #Renderiza el template del formulario para agregar un proveedor
+    if request.method == 'GET':
+        
+        url = URLFrp + 'projects/' + str(provider_id)
+        r = requests.get( url) 
+        reqJ = r.json()
+
+        #Obtiene todos contracts para el select
+        urlCountContracts = URLFrp + 'contracts/count'
+        countAmountContracts = requests.get( urlCountContracts ).json()['count']
+
+        queryStrContracts = 'contracts/?offset=0&limit=' + str(countAmountContracts)
+        urlContracts = URLFrp + queryStrContracts
+
+        rContracts = requests.get( urlContracts ) 
+        contracts = rContracts.json() 
+        
+        print(reqJ)
+        return render_template( 'projects/edit.html', data = reqJ, catalog = 'projects', menu = general.menuProject, contracts = contracts )
+    
+    #Cuando termina de cargar la pagina el javascrip pide la lista de los proveedores
+    elif request.method == 'POST':
+        data = request.get_json()
+        
+        idRegister = data['id']
+
+        dicData = { 
+            'title'         : data['title'         ], 
+            'description'   : data['description'   ], 
+            'city'          : data['city'          ], 
+            'category'      : data['category'      ], 
+            'department'    : data['department'    ], 
+            'budget'        : data['budget'        ], 
+            'contract'      : data['contract'      ], 
+            'planed_kickoff': data['planed_kickoff'], 
+            'planed_ending' : data['planed_ending' ], 
+            'inceptor_uuid' : data['inceptor_uuid' ], 
+        }
+
+        dataJSON = json.dumps(dicData)
+
+        url = URLFrp + 'projects/' + idRegister
+        r = requests.put( url, data=dataJSON)
+
+        return jsonify( {'success':'success'} )
+
+    elif request.method == 'DELETE':
+        url = URLFrp + 'projects/' + str(provider_id)
+        r = requests.delete( url )
+        return jsonify( 'yes' )
 
 
 
