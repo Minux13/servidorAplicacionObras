45a17fdb6f91667405d9aae1596774ad82a568bd
Miriam <miriam.physics@gmail.com>
2019-06-29 18:25:39 -0500

    Tableros enlaza a la url graphics y se cambio la fecha grafica

--- general.py
+++ general.py
@@ -6,7 +6,7 @@ menuContract = [
     {'name':'Contratos',  'url':'/contracts ', 'idHTML':'menuComponentContracts', 'iconClass':'fas fa-file-contract', 'isActive':'active'          },
     {'name':'Obras',      'url':'/projects ' , 'idHTML':'menuComponentProjects' , 'iconClass':'fas fa-hard-hat'     , 'isActive':''                },
     {'name':'Avance de Obras','url':'/follow_ups/projects', 'idHTML':'menuComponentFollowUp' , 'iconClass':'fas fa-business-time', 'isActive':''           },
-    {'name':'Tableros',   'url':'http://minux.pw/frontSistemaDeObras/', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''                },
+    {'name':'Tableros',   'url':'/graphics', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''                },
 ]
 
 
@@ -16,7 +16,7 @@ menuProvider = [
     {'name':'Contratos',  'url':'/contracts'  , 'idHTML':'menuComponentContracts', 'iconClass':'fas fa-file-contract', 'isActive':''                },
     {'name':'Obras',      'url':'/projects'   , 'idHTML':'menuComponentProjects' , 'iconClass':'fas fa-hard-hat'     , 'isActive':''                },
     {'name':'Avance de Obras','url':'/follow_ups/projects', 'idHTML':'menuComponentFollowUp' , 'iconClass':'fas fa-business-time', 'isActive':''           },
-    {'name':'Tableros',   'url':'http://minux.pw/frontSistemaDeObras/'  , 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''                },
+    {'name':'Tableros',   'url':'/graphics'  , 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''                },
 ]
 
 
@@ -26,7 +26,7 @@ menuProject = [
     {'name':'Contratos',      'url':'/contracts', 'idHTML':'menuComponentContracts', 'iconClass':'fas fa-file-contract', 'isActive':''           },
     {'name':'Obras',          'url':'/projects' , 'idHTML':'menuComponentProjects' , 'iconClass':'fas fa-hard-hat'     , 'isActive':'active'     },
     {'name':'Avance de Obras','url':'/follow_ups/projects', 'idHTML':'menuComponentFollowUp' , 'iconClass':'fas fa-business-time', 'isActive':''           },
-    {'name':'Tableros',       'url':'http://minux.pw/frontSistemaDeObras', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''           },
+    {'name':'Tableros',       'url':'/graphics', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''           },
 ]
 
 
@@ -36,7 +36,7 @@ menuFollowUps = [
     {'name':'Contratos',      'url':'/contracts', 'idHTML':'menuComponentContracts', 'iconClass':'fas fa-file-contract', 'isActive':''           },
     {'name':'Obras',          'url':'/projects' , 'idHTML':'menuComponentProjects' , 'iconClass':'fas fa-hard-hat'     , 'isActive':''           },
     {'name':'Avance de Obras','url':'/follow_ups/projects', 'idHTML':'menuComponentFollowUp' , 'iconClass':'fas fa-business-time', 'isActive':'active'     },
-    {'name':'Tableros',       'url':'http://minux.pw/frontSistemaDeObras', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''           },
+    {'name':'Tableros',       'url':'/graphics', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':''           },
 ]
 
 menuGraphics = [
@@ -45,7 +45,7 @@ menuGraphics = [
     {'name':'Contratos',      'url':'/contracts', 'idHTML':'menuComponentContracts', 'iconClass':'fas fa-file-contract', 'isActive':''           },
     {'name':'Obras',          'url':'/projects' , 'idHTML':'menuComponentProjects' , 'iconClass':'fas fa-hard-hat'     , 'isActive':''           },
     {'name':'Avance de Obras','url':'/follow_ups/projects', 'idHTML':'menuComponentFollowUp' , 'iconClass':'fas fa-business-time', 'isActive':''     },
-    {'name':'Tableros',       'url':'http://minux.pw/frontSistemaDeObras', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':'active'           },
+    {'name':'Tableros',       'url':'/graphics', 'idHTML':'menuComponentGraphics' , 'iconClass':'fas fa-chart-pie'    , 'isActive':'active'           },
 ]
 
 
